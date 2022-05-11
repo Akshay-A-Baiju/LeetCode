029768f0cb185f2a5e9f2b32f850f15803a9f26f
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2022-05-11 18:45:50 +0530

    Time: 31 ms (5.38%), Space: 10.9 MB (5.00%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int fun(int x, int y, vector <vector<int>> &triangle, map <pair<int,int>,int> &dp)
+    {
+        if (x==0 && y==0)
+            return triangle[0][0];
+        if (x<0 || y<0)
+            return 1e9;
+        if (dp.find({x,y})!=dp.end())
+            return dp[{x,y}];
+        int path=1e9;
+        if (y<triangle[x-1].size())
+            path=min(path,fun(x-1,y,triangle,dp));
+        if (y-1>=0)
+            path=min(path,fun(x-1,y-1,triangle,dp));
+        return dp[{x,y}]=triangle[x][y]+path;
+    }
+    int minimumTotal(vector<vector<int>>& triangle)
+    {
+        map <pair<int,int>,int> dp;
+        int n=triangle.size();
+        int ans=1e9;
+        for (int i=0;i<n;i++)
+            ans=min(ans,fun(n-1,i,triangle,dp));
+        return ans;
+    }
+};
