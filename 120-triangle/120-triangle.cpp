a9f0a2a4d04e74e1133b4fefa06736c3c29a9e81
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2022-05-12 12:46:09 +0530

    Time: 13 ms (20.03%), Space: 9 MB (24.58%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -7,7 +7,7 @@ public:
         vector <vector <int>> dp(n, vector <int> (n,0));
         for (int i=n-1;i>=0;i--)
         {
-            for (int j=0;j<=i;j++)
+            for (int j=i;j>=0;j--)
             {
                 if (i==n-1)
                     dp[i][j]=triangle[i][j];
