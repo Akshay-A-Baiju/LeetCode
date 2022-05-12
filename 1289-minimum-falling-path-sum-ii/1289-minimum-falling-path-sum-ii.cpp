0647f2c5e2a4afec3420e615d6f43bcf83e652f1
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2022-05-12 13:22:35 +0530

    Time: 312 ms (53.11%), Space: 15.8 MB (43.32%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int fun(int i, int j, vector <vector <int>> &matrix, vector <vector<int>> &dp)
-    {
-        // returns min path sum to reach cell (i,j) from top row
-        int n=matrix.size();
-        if (j<0 || j>=n)
-            return 1e9;
-        if (i==0)
-            return matrix[i][j];
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        int add=1e9;
-        for (int k=0;k<n;k++)
-        {
-            if (j!=k)
-                add=min(add,fun(i-1,k,matrix,dp));
-        }
-        return dp[i][j]=matrix[i][j]+add;
-    }
-    
     int minFallingPathSum(vector<vector<int>>& grid)
     {
         int n=grid.size();
-        vector <vector <int>> dp(n, vector <int> (n,-1));
+        vector <vector <int>> dp(n, vector <int> (n,0));
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (i==0)
+                    dp[i][j]=grid[i][j];
+                else
+                {
+                    int add=1e9;
+                    for (int k=0;k<n;k++)
+                    {
+                        if (k!=j)
+                            add=min(add,dp[i-1][k]);
+                    }
+                    dp[i][j]=grid[i][j]+add;
+                }
+            }
+        }
         int ans=1e9;
         for (int i=0;i<n;i++)
-            ans=min(ans,fun(n-1,i,grid,dp));
-        return ans;
+            ans=min(ans,dp[n-1][i]);
+        return ans;        
     }
 };
