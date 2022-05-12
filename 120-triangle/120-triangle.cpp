69bc84963b720917700a1267ee29a6f905483177
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2022-05-12 12:38:45 +0530

    Time: 9 ms (45.31%), Space: 9.1 MB (10.03%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int fun(int x, int y, vector <vector<int>> &triangle, vector <vector<int>> &dp)
-    {
-        if (x==0 && y==0)
-            return triangle[0][0];
-        if (x<0 || y<0)
-            return 1e9;
-        if (dp[x][y]!=-1)
-            return dp[x][y];
-        int path=1e9;
-        if (y<triangle[x-1].size())
-            path=min(path,fun(x-1,y,triangle,dp));
-        if (y-1>=0)
-            path=min(path,fun(x-1,y-1,triangle,dp));
-        return dp[x][y]=triangle[x][y]+path;
-    }
-    int minimumTotal(vector<vector<int>>& triangle)
+    int fun(int i, int j, vector <vector <int>> &triangle, vector <vector <int>> &dp)
     {
         int n=triangle.size();
-        vector <vector<int>> dp(n, vector <int> (n,-1));
-        int ans=1e9;
-        for (int i=0;i<n;i++)
-            ans=min(ans,fun(n-1,i,triangle,dp));
-        return ans;
+        if (i==n-1)
+            return triangle[i][j];
+        if (dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=triangle[i][j]+min(fun(i+1,j,triangle,dp),fun(i+1,j+1,triangle,dp));
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle)
+    {
+        // single source, variable ending
+        int n=triangle.size();
+        vector <vector <int>> dp(n, vector <int> (n,-1));
+        return fun(0,0,triangle,dp);
     }
 };
